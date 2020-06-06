752f6edf6b32c2f437b14d821dba1ba42c3db93b
Thomas Kastanek <28022361+tckastanek@users.noreply.github.com>
2020-06-06 15:51:20 -0400

    naive imp kinda works

---
+++ src/main.rs
@@ -0,0 +1,121 @@
+use rustyline::{error::ReadlineError, Editor};
+
+struct Parser {
+    input: String,
+    tokens: Option<Vec<Token>>,
+}
+
+impl Parser {
+    pub fn new(input: String) -> Self {
+        Self {
+            input,
+            tokens: None,
+        }
+    }
+
+    pub fn tokenize(mut self) -> Self {
+        let mut tokens: Vec<Token> = Vec::new();
+
+        for raw_token in self.input.split(" ") {
+            let token = Token::from_str(raw_token);
+
+            tokens.push(token);
+        }
+
+        self.tokens = Some(tokens);
+
+        self
+    }
+
+    pub fn parse(self) -> f32 {
+        if let Some(tokens) = self.tokens {
+            let mut lhs = None;
+            let mut op = None;
+
+            for token in tokens {
+                match token {
+                    Token::Float(float) => match (lhs, &op) {
+                        (None, _) => lhs = Some(float),
+                        (Some(lhs_val), Some(op_token)) => match op_token {
+                            Token::Plus => {
+                                lhs = Some(lhs_val + float);
+                                op = None;
+                            }
+                            Token::Minus => {
+                                lhs = Some(lhs_val - float);
+                                op = None;
+                            }
+                            _ => unimplemented!(),
+                        },
+                        (_, _) => unimplemented!(),
+                    },
+                    Token::Plus => match (lhs, &op) {
+                        (Some(_), None) => op = Some(Token::Plus),
+                        (_, _) => unimplemented!(),
+                    },
+                    Token::Minus => match (lhs, &op) {
+                        (Some(_), None) => op = Some(Token::Minus),
+                        (_, _) => unimplemented!(),
+                    },
+                }
+            }
+
+            // eval
+            lhs.unwrap()
+        } else {
+            unimplemented!();
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Token {
+    Float(f32),
+    Plus,
+    Minus,
+}
+
+impl Token {
+    pub fn from_str(input: &str) -> Self {
+        // let float = input.parse::<f32>().unwrap();
+        if let Ok(float) = input.parse::<f32>() {
+            Token::Float(float)
+        } else {
+            match input {
+                "+" => Token::Plus,
+                "-" => Token::Minus,
+                _ => unimplemented!(),
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut rl = Editor::<()>::new();
+
+    loop {
+        let readline = rl.readline(">> ");
+
+        match readline {
+            Ok(line) => {
+                let parser = Parser::new(line);
+
+                let result = parser.tokenize().parse();
+
+                println!("Line: {:#?}", result);
+            }
+            Err(ReadlineError::Interrupted) => {
+                eprintln!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                eprintln!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                eprintln!("Error: {}", err);
+                break;
+            }
+        }
+    }
+}
